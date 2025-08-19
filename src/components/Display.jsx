88b5e58ea73cb8fa6cab905abeb12f3181b58999
eyasq <qeremeyas99@gmail.com>
2025-08-19 14:30:07 +0300

    commit before adding router and potentially breaking things

--- src/components/Display.jsx
+++ src/components/Display.jsx
@@ -3,13 +3,27 @@ export default function Display({todos, setTodos}){
     function handleRemove(id){
         setTodos((prevTodos)=>prevTodos.filter((todo)=>todo.id!==id))
     }
+    function handleCheck(id){
+        setTodos((prevTodos)=>
+        prevTodos.map((todo)=>
+        todo.id==id?{...todo, completed:!todo.completed}:todo))
+    }
 return(
 
     <>  
     <p>Current Todos:</p>
     <ul>
-        {todos.map((item)=><li style={{textAlign:"left", color: item.important? "red":"black"
-        }} key={item.id}>{item.text} - {item.id}  <button onClick={()=>{handleRemove(item.id)}}  style={{fontSize:"0.75rem"}}>X </button></li>)}
+        {todos.map((item)=><li 
+        style={{textAlign:"left", color:item.completed? "green":(item.important? "red":"black"),
+        }}
+         key={item.id}>{item.text} 
+         -  
+         <button 
+         onClick={()=>{handleRemove(item.id)}}  
+         style={{fontSize:"0.75rem"}}>X </button> 
+         <input type="checkbox" onChange={()=>handleCheck(item.id)} />
+         </li>
+        )}
     </ul>
     </>
 
